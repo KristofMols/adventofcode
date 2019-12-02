159ee77ed05258ac2dcd58312b2928931e50f8ed
Kristof Mols <kristof.mols@aca-it.be>
2019-12-02 14:49:40 +0100

    result for day 2

--- day_2/day2-2.lua
+++ day_2/day2-2.lua
@@ -1,3 +1,11 @@
 require "day_2.day2-1"
 
-calculate_program(12, 2)
+for noun = 0, 99 do 
+    for verb = 0, 99 do 
+        local result = calculate_program(noun, verb)
+        if result == 19690720 then
+            print("DAY 2.2 : " .. (100 * noun + verb))
+            return
+        end
+    end 
+end
