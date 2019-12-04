5f5a15c404f67e39466e622f250493d9897c3f68
Kristof Mols <kristof.mols@aca-it.be>
2019-12-04 14:14:32 +0100

    add results for day 4

---
+++ day_4/day4-1.lua
@@ -0,0 +1,36 @@
+local low_range = 172930
+local high_range = 683082
+
+function check_increasing(num)
+    local last_digit = nil
+
+    for digit in string.gmatch(num, "%d") do
+        if last_digit ~= nil and last_digit > digit then return false end
+
+        last_digit = digit
+    end
+
+    return true
+end
+
+function countSubstring(s1, s2)
+    return select(2, tostring(s1):gsub(s2, ""))
+end
+
+function check_multiple_digit(num)
+    for digit in string.gmatch(num, "%d") do
+        if countSubstring(num, digit) > 1 then return true end
+    end
+
+    return false
+end
+
+local total_matches = 0
+
+for count = 172930, 683082, 1 do
+    if check_increasing(count) and check_multiple_digit(count) then
+        total_matches = total_matches + 1
+    end
+end
+
+print("DAY 4.1 : " .. math.floor(total_matches))
---
+++ day_4/day4-2.lua
@@ -0,0 +1,20 @@
+require "day_4.day4-1"
+
+local total_matches = 0
+
+function check_double_count(num)
+    for digit in string.gmatch(num, "%d") do
+        local count = countSubstring(num, digit)
+        if count == 2 then return true end
+    end
+
+    return false
+end
+
+for count = 172930, 683082, 1 do
+    if check_increasing(count) and check_double_count(count) then
+        total_matches = total_matches + 1
+    end
+end
+
+print("DAY 4.2 : " .. math.floor(total_matches))
