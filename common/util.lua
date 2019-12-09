ec568a7f810107b01d2669ecb65082c7dba4ee20
Kristof Mols <kristof.mols@aca-it.be>
2019-12-09 10:52:25 +0100

    result for day 5

--- common/util.lua
+++ common/util.lua
@@ -3,3 +3,9 @@ function split(str, pat)
     str:gsub(pat, function(x) tbl[#tbl + 1] = x end)
     return tbl
 end
+
+function split_num(str, pat)
+local tbl = {}
+str:gsub(pat, function(x) tbl[#tbl + 1] = tonumber(x) end)
+return tbl
+end
--- day_5/day5-1.lua
+++ day_5/day5-1.lua
@@ -1,7 +1,8 @@
 require "common.file"
 require "common.util"
 
-local stored_value = 1
+local operation_count = 0
+local stored_value = 5
 local result = nil
 
 function get_4_items(tab, start)
@@ -13,7 +14,7 @@ function get_modes(code)
     while code ~= 0 do
         lastDigit = code % 10
         code = math.floor(code / 10)
-    
+
         modes[#modes + 1] = math.floor(lastDigit)
     end
 
@@ -21,43 +22,77 @@ function get_modes(code)
 end
 
 function exec_opcode(tab, opcode, first, second, res)
-    print(opcode .. '|' .. first .. '|' .. second .. '|' .. res)
     local modes = get_modes(opcode)
 
     if modes[1] == 1 then
-        if modes[3] == nil or modes[3] == 0 then
-            first = tab[first + 1]
-        end
-    
-        if modes[4] == nil or modes[4] == 0 then
-            second = tab[second + 1]
-        end
+        if modes[3] == nil or modes[3] == 0 then first = tab[first + 1] end
+        if modes[4] == nil or modes[4] == 0 then second = tab[second + 1] end
+        
         local result = math.floor(first + second)
         tab[res + 1] = result
-        return 4
+
+        operation_count = operation_count + 4
     elseif modes[1] == 2 then
-        if modes[3] == nil or modes[3] == 0 then
-            first = tab[first + 1]
-        end
-    
-        if modes[4] == nil or modes[4] == 0 then
-            second = tab[second + 1]
-        end
+        if modes[3] == nil or modes[3] == 0 then first = tab[first + 1] end
+        if modes[4] == nil or modes[4] == 0 then second = tab[second + 1] end
+        
         local result = math.floor(first * second)
         tab[res + 1] = result
-        return 4
+
+        operation_count = operation_count +  4
     elseif modes[1] == 3 then
         tab[first + 1] = stored_value
-        return 2
+
+        operation_count = operation_count +  2
     elseif modes[1] == 4 then
-        result = tab[first + 1]
-        return 2
+        if modes[3] == nil or modes[3] == 0 then first = tab[first + 1] end
+        result = first
+
+        operation_count = operation_count +  2
+    elseif modes[1] == 5 then
+        if modes[3] == nil or modes[3] == 0 then first = tab[first + 1] end
+        if modes[4] == nil or modes[4] == 0 then second = tab[second + 1] end
+
+        if first ~= 0 then
+            operation_count = second
+        else
+            operation_count = operation_count +  3
+        end
+    elseif modes[1] == 6 then
+        if modes[3] == nil or modes[3] == 0 then first = tab[first + 1] end
+        if modes[4] == nil or modes[4] == 0 then second = tab[second + 1] end
+
+        if first == 0 then
+            operation_count = second
+        else
+            operation_count = operation_count +  3
+        end
+    elseif modes[1] == 7 then
+        if modes[3] == nil or modes[3] == 0 then first = tab[first + 1] end
+        if modes[4] == nil or modes[4] == 0 then second = tab[second + 1] end
+
+        if first < second then
+            tab[res + 1] = 1
+        else
+            tab[res + 1] = 0
+        end
+
+        operation_count = operation_count +  4
+    elseif modes[1] == 8 then
+        if modes[3] == nil or modes[3] == 0 then first = tab[first + 1] end
+        if modes[4] == nil or modes[4] == 0 then second = tab[second + 1] end
+
+        if first == second then
+            tab[res + 1] = 1
+        else
+            tab[res + 1] = 0
+        end
+
+        operation_count = operation_count +  4
     elseif modes[1] == 99 then
-        -- print("STOP")
-        return 1000
+        operation_count = 1000
     else
-        -- print("ERROR")
-        return 1000
+        operation_count = 1000
     end
 end
 
@@ -66,16 +101,15 @@ local lines = lines_from(file)
 
 function calculate_program()
     for _, line in pairs(lines) do
-        local items = split(line, "[^,]*")
-        local operation_count = 0
+        local items = split_num(line, "[^,]*")
 
         while operation_count <= #items do
             opcode, first, second, res = get_4_items(items, operation_count)
-            operation_count = operation_count + exec_opcode(items, opcode, first, second, res)
+            exec_opcode(items, opcode, first, second, res)
         end
     end
 end
 
 calculate_program()
 
-print("DAY 5.1 : " .. result)
+print("DAY 5 : " .. result)
