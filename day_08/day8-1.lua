20ab72a9b58680975e8da25826f213b0a03ba2ad
Kristof Mols <kristof.mols@gmail.com>
2020-02-04 13:54:18 +0100

    add fix for strings ending with \\" and create blacklist for chars to escape

--- day_08/day8-1.lua
+++ day_08/day8-1.lua
@@ -6,20 +6,28 @@ local lines = lines_from(file)
 local total_count = 0
 local memory_count = 0
 
+local chars_to_escape = {
+    {regex = "\\x%x%x", length = 3}, 
+    {regex = "\\\\", length = 1},
+    {regex = "\\\"", length = 1}
+}
+
 function count_line_value(line)
     local line_total_count = line:len()
     local line_memory_count = line_total_count - 2
     
-    for word in string.gmatch(line, "\\x%x%x") do line_memory_count = line_memory_count - 3 end
-    for word in string.gmatch(line, "\\[\\|\"]") do line_memory_count = line_memory_count - 1 end
+    line = line:sub(2, line:len() - 1)
 
-    return line_total_count, line_memory_count
-end
+    for k, v in pairs(chars_to_escape) do
+        for word in string.gmatch(line, v.regex) do
+            line_memory_count = line_memory_count - v.length
+        end
+    end
 
-for _, line in pairs(lines) do 
-    line_total_count, line_memory_count = count_line_value(line)
     total_count = total_count + line_total_count
     memory_count = memory_count + line_memory_count
 end
 
-print("Day 8", total_count - memory_count)
+for _, line in pairs(lines) do count_line_value(line) end
+
+print("Day 8.1", total_count - memory_count)
