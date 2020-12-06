af9aa2cf587303ece92058a03fa5d98269000f35
KristofMols <kristof.mols@gmail.com>
2020-12-06 20:15:18 +0100

    initial commit for day 3

---
+++ day_03/day_03-2.lua
@@ -0,0 +1,12 @@
+require 'common.file'
+
+M = require 'lib.Moses'
+
+local lines = lines_from('day_03/day_03-input.txt')
+
+local result = 0
+
+M.each(lines, function(v1, i1) 
+end)
+
+print('day_03-2', result)
