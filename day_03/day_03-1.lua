ba267aa8d5f4d222b53fbacc06083aa4e7527cb0
KristofMols <kristof.mols@gmail.com>
2020-12-07 18:09:17 +0100

    update solution for day 3

--- day_03/day_03-1.lua
+++ day_03/day_03-1.lua
@@ -4,9 +4,22 @@ M = require 'lib.Moses'
 
 local lines = lines_from('day_03/day_03-input.txt')
 
+local input_map = {}
 local result = 0
 
-M.each(lines, function(v1, i1) 
+M.each(lines, function(v1, i1)
+    input_map[i1] = {}
+    for i = 1, #v1 do
+        input_map[i1][i] = v1:sub(i, i)
+    end
 end)
+
+x_pos = 1
+
+for i = 1, #input_map do
+    if input_map[i][x_pos] == '#' then result = result + 1 end
+    x_pos = x_pos + 3
+    if x_pos > #input_map[i] then x_pos = x_pos - #input_map[i] end
+end
 
 print('day_03-1', result)
